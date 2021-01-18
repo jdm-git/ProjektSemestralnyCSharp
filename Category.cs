8b3d6016dd7a36c8a62c77efc7ee787719b99501
jdm-git <58112825+jdm-git@users.noreply.github.com>
2021-01-18 12:07:50 +0100

    Added Category.cs and Product.cs

---
+++ Category.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjektSemestralnyCSharp
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Order.cs
+++ Order.cs
@@ -5,12 +5,12 @@ namespace ProjektSemestralnyCSharp
     public class Order
     {
         public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int ProductId { get; set; }
         public DateTime OrderDate { get; set; }
         public DateTime OrderPickUp { get; set; }
         public DateTime OrderReturn { get; set; }
-        public int ClientId { get; set; }
-        public int ProductId { get; set; }
         public virtual Client Client { get; set; }
-        //public Product Product{ get; set; }
+        public Product Product{ get; set; }
     }
 }
