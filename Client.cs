6f7e5fd22825931ad87d8298e24d17969e0fec40
jdm-git <58112825+jdm-git@users.noreply.github.com>
2021-01-18 11:42:43 +0100

    Added Client.cs

---
+++ Client.cs
@@ -0,0 +1,17 @@
+﻿namespace ProjektSemestralnyCSharp
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+        public string ZipCode { get; set; }
+        public string Email { get; set; }
+
+    }
+}
